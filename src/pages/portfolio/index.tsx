f6455099abe2be8b9e4ca0a04c9f26db60e55b5a
ShihTingJustin <justinhuang777@gmail.com>
2022-10-29 18:29:59 +0800

    feat: add portfolio page

---
+++ src/pages/portfolio/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Layout from '@theme/Layout';
+
+export default function Portfolio(): JSX.Element {
+  return (
+    <Layout title={'title'} description={'desc'}>
+      <main className="margin-vert--lg">
+        123
+        {/* <ShowcaseHeader />
+        <ShowcaseFilters />
+        <ShowcaseCards /> */}
+      </main>
+    </Layout>
+  );
+}
