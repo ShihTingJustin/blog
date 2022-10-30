56a1d0e5144559386b8aa5927266425d28cb5919
ShihTingJustin <justinhuang777@gmail.com>
2022-10-30 21:49:45 +0800

    feat: update home page metaData

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ function HomepageHeader() {
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <Layout title={`home`} description={siteConfig.description}>
+    <Layout title={translate({ id: 'home.page.title' })} description={siteConfig.description}>
       <Head>
         <meta
           name="viewport"
