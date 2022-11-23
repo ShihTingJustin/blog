b5b31bd3b79b2ad544493d4a0cf123b0da85ebde
ShihTingJustin <justinhuang777@gmail.com>
2022-11-23 22:30:43 +0800

    feat: add path and hover style in Feature component

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,28 +2,34 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.scss';
 import Translate from '@docusaurus/Translate';
+import { useHistory } from '@docusaurus/router';
 
 const FeatureList = [
   {
     title: <Translate id="body.feature.title.left">Technical note</Translate>,
     Svg: require('@site/static/img/code_review_dark.svg').default,
     description: <Translate id="body.feature.content.left"></Translate>,
+    path: '/docs/intro',
   },
   {
     title: <Translate id="body.feature.title.center">Articles</Translate>,
     Svg: require('@site/static/img/programming_dark.svg').default,
     description: <Translate id="body.feature.content.center"></Translate>,
+    path: '/blog',
   },
   {
     title: <Translate id="body.feature.title.right">Portfolio</Translate>,
     Svg: require('@site/static/img/responsive_dark.svg').default,
     description: <Translate id="body.feature.content.right"></Translate>,
+    path: '/portfolio',
   },
 ];
 
-function Feature({ Svg, title, description, to }) {
+function Feature({ Svg, title, description, path }) {
+  const history = useHistory();
+
   return (
-    <div className={clsx('col col--4')}>
+    <div className={`${styles.feature} ${clsx('col col--4')}`} onClick={() => history.push(path)}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
